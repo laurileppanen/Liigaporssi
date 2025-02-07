a0a570030d611a73ba032a37ac0455b38d3332de
Lauri Leppänen <late-22-@hotmail.com>
2025-02-08 01:33:02 +0200

    All players fetched to their teams

--- liigaporssi/liigaporssi/hae_pelaajat.py
+++ liigaporssi/liigaporssi/hae_pelaajat.py
@@ -19,7 +19,6 @@ DB_PATH = settings.DATABASES['default']['NAME']
 URL = "https://liigaporssi.fi/sm-liiga/joukkueet/hifk/pelaajat"
 
 URL2 = "https://liigaporssi.fi/sm-liiga"
-TEAM_URL = URL2 + "/joukkueet/{}/pelaajat"
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
@@ -33,8 +32,6 @@ driver.get(URL2 + "/sarjataulukko")
 time.sleep(3)
 soup2 = BeautifulSoup(driver.page_source, 'html.parser')
 
-driver.quit()
-
 joukkueet = []
 for td in soup2.find_all('td', class_='essential'):
     strong_tag = td.find('strong')
@@ -43,15 +40,11 @@ for td in soup2.find_all('td', class_='essential'):
         if not joukkue.isdigit():
             joukkueet.append(joukkue)
 
-
-print(f"JOUKKUEET: {joukkueet}")
-
 conn = sqlite3.connect(DB_PATH)
 cursor = conn.cursor()
 
 for joukkue in joukkueet:
     taulun_nimi = f"{joukkue.replace('ä', 'a').replace('Ä', 'A').replace('-', '').replace(' ', '_')}_pelaajat"
-    print(f"TAULU: {taulun_nimi}")
 
     cursor.execute(f"DROP TABLE IF EXISTS {taulun_nimi}")
     
@@ -62,48 +55,46 @@ for joukkue in joukkueet:
     )
     ''')
 
-maalivahdit_taulukko = soup.find('div', id='stats_m_168761288')
-puolustajat_taulukko = soup.find('div', id='stats_p_168761288')
-hyokkaajat_taulukko = soup.find('div', id='stats_h_168761288')
+    url_joukkue = joukkue.replace('ä', 'a').replace('Ä', 'A').lower()
+    TEAM_URL = URL2 + f"/joukkueet/{url_joukkue}/pelaajat"
+    driver.get(TEAM_URL)
 
-maalivahti_nimi = maalivahdit_taulukko.find_all('td', class_='player_name essential persist')
-puolustajat_nimi = puolustajat_taulukko.find_all('td', class_='player_name')
-hyokkaajat_nimi = hyokkaajat_taulukko.find_all('td', class_='player_name')
+    time.sleep(3)
+    soup3 = BeautifulSoup(driver.page_source, 'html.parser')
 
-maalivahdit =[]
-for nimi in maalivahti_nimi:
-    maalivahti = nimi.find('a', class_='player_link').text.strip()
-    maalivahdit.append(maalivahti)
+    maalivahdit_taulukko = soup3.find('div', id=lambda x: x and x.startswith("stats_m_"))
+    puolustajat_taulukko = soup3.find('div', id=lambda x: x and x.startswith("stats_p_"))
+    hyokkaajat_taulukko = soup3.find('div', id=lambda x: x and x.startswith("stats_h_"))
 
-puolustajat = []
-for nimi in puolustajat_nimi:
-    puolustaja = nimi.find('a', class_='player_link').text.strip()
-    puolustajat.append(puolustaja)
+    maalivahti_nimi = maalivahdit_taulukko.find_all('td', class_='player_name essential persist')
+    puolustajat_nimi = puolustajat_taulukko.find_all('td', class_='player_name')
+    hyokkaajat_nimi = hyokkaajat_taulukko.find_all('td', class_='player_name')
 
-hyokkaajat = []
-for nimi in hyokkaajat_nimi:
-    hyokkaaja = nimi.find('a', class_='player_link').text.strip()
-    hyokkaajat.append(hyokkaaja)
+    maalivahdit =[]
+    for nimi in maalivahti_nimi:
+        maalivahti = nimi.find('a', class_='player_link').text.strip()
+        maalivahdit.append(maalivahti)
+
+    puolustajat = []
+    for nimi in puolustajat_nimi:
+        puolustaja = nimi.find('a', class_='player_link').text.strip()
+        puolustajat.append(puolustaja)
+
+    hyokkaajat = []
+    for nimi in hyokkaajat_nimi:
+        hyokkaaja = nimi.find('a', class_='player_link').text.strip()
+        hyokkaajat.append(hyokkaaja)
+
+    for nimi in maalivahdit:
+      cursor.execute(f"INSERT OR IGNORE INTO {taulun_nimi} (nimi) VALUES (?)", (nimi,))
+
+    for nimi in puolustajat:
+      cursor.execute(f"INSERT OR IGNORE INTO {taulun_nimi} (nimi) VALUES (?)", (nimi,))
+
+    for nimi in hyokkaajat:
+      cursor.execute(f"INSERT OR IGNORE INTO {taulun_nimi} (nimi) VALUES (?)", (nimi,))    
 
 
-cursor.execute("DROP TABLE IF EXISTS HIFK_pelaajat")
-
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS HIFK_pelaajat(
-               pelaaja_id INTEGER PRIMARY KEY AUTOINCREMENT,
-               nimi TEXT NOT NULL)   
-''')
-
-
-
-for nimi in maalivahdit:
-    cursor.execute("INSERT OR IGNORE INTO HIFK_pelaajat (nimi) VALUES (?)", (nimi,))
-
-for nimi in puolustajat:
-    cursor.execute("INSERT OR IGNORE INTO HIFK_pelaajat (nimi) VALUES (?)", (nimi,))
-
-for nimi in hyokkaajat:
-    cursor.execute("INSERT OR IGNORE INTO HIFK_pelaajat (nimi) VALUES (?)", (nimi,))
-
+driver.quit()
 conn.commit()
 conn.close()    
